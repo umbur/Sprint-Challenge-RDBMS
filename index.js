0361b85cb1a14968e5bd6ab6678eb56e0888a97b
umbur <work1415@gmail.com>
2019-03-01 11:56:12 -0700

    created/configured routes using express router, almost completed MVP

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const knex = require('knex');
 
+
 const server = express();
 const PORT = 5000;
 
+const actionRoutes = require('./routes/actionRoutes')
+const projectRoutes = require('./routes/projectRoutes');
+
 server.use(express.json());
+
+server.use('/api/actions', actionRoutes)
+server.use('/api/projects', projectRoutes);
 
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
